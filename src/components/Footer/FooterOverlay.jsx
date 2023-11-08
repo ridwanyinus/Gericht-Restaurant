162b50089310e3aba9c688618174e9a4871745b7
officialyaro9 <ridwanyinusa15@gmail.com>
2023-11-08 10:04:44 +0100

    second commit

--- src/components/Footer/FooterOverlay.jsx
+++ src/components/Footer/FooterOverlay.jsx
@@ -5,7 +5,7 @@ import "./FooterOverlay.css";
 const FooterOverlay = () => (
    <div className="app__footerOverlay">
       <div className="app__footerOverlay-black"></div>
-      <div className="app__footerOverlay-img app__bg"></div>
+      <div className="app__footerOverlay-img app__bg "></div>
    </div>
 );
 
